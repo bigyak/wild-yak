347865726b3e279657d9f577571b3c85353438f6
Hemchand Thalanchery <hemchand@Hemchands-MacBook-Pro-2.local>
2016-06-04 16:37:18 +0530

    Adding postback support for FB

--- src/formatters/fb.js
+++ src/formatters/fb.js
@@ -6,9 +6,17 @@ export function mergeIncomingMessages<TMessage : MessageType>(messages: Array<Ob
 }
 
 export function parseIncomingMessage<TMessage : MessageType>(message: Object) : TMessage {
-  throw new Error("Not implemented");
+  const _message: TMessage = message;
+  if (message.postback) {
+    _message.isPostback = true;
+    _message.text = message.postback.payload;
+  } else {
+    _message.isPostback = false;
+  }
+  return _message;
 }
 
 export function formatOutgoingMessage<TMessage : MessageType>(message: Object) : TMessage {
-  throw new Error("Not implemented");
+  const _message: TMessage = message;
+  return _message;
 }
